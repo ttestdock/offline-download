ed1de378e4a50a9e583f2abcc1b888ed199635fe
ttestdock <2718648523@qq.com>
2016-11-28 18:43:50 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'https://officed2wn.herokuapp.com/';
+$config['base_url'] = 'https://officed4wn.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
