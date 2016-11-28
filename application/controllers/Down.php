80fb8bf4700ca890accca1808c45d9a856077d03
ttestdock <2718648523@qq.com>
2016-11-28 15:53:19 +0800

    Update Down.php

--- application/controllers/Down.php
+++ application/controllers/Down.php
@@ -6,3 +6,7 @@ class Down extends CI_Controller {
     public function index()
     {
         $url = $this->input->get('url');
+            shell_exec("wget -b -nc -o /dev/null --restrict-file-names=nocontrol -P ./download " . escapeshellarg($url));
+            echo "success";
+    }
+}
